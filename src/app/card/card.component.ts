c25dbf7a714174bd26b2b438f3804308e3dc1206
Ashish Kumar <ashish.kumar@bitloka.com>
2024-08-28 11:29:57 +0530

    navigation-bar

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,9 +11,26 @@ import { TextboxState } from '../store/reducer';
 export class CardComponent {
   currentValue$: Observable<string | number | null>;
   values$: Observable<Array<string | number>>;
+  navBar: string[] = [];
+
+  student = ['Home', 'Study'];
+  teacher = ['LMS', 'Dashboard', 'Exams'];
+  clerk = ['Fees', 'Admission', 'Result'];
 
   constructor(private store: Store<{ textbox: TextboxState }>) {
     this.currentValue$ = store.select(state => state.textbox.currentValue);
     this.values$ = store.select(state => state.textbox.values);
+  }
+
+  onStudentClick() {
+    this.navBar = this.student; 
+  }
+
+  onTeacherClick() {
+    this.navBar = this.teacher; 
+  }
+
+  onClerkClick() {
+    this.navBar = this.clerk; 
   }
 }
